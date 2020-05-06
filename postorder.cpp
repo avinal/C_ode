1eef4cb15d163550ac3e37029c264909d7c5f0de
Avinal <avinal.xlvii@gmail.com>
2020-05-06 10:53:00 +0530

    post order traversal of tree

---
+++ postorder.cpp
@@ -0,0 +1,53 @@
+/*
+D:\C\C_ode\postorder.cpp
+Avinal Kumar
+May 06, 2020
+Creating a binary search tree.
+PostOrder traversal of tree without using recursion
+*/
+#include <iostream>
+
+struct node
+{
+    node *left;
+    int data;
+    node *right;
+    bool traversed;
+    node(int d = 0)
+        : left(nullptr), right(nullptr), data(d), traversed(false) {}
+};
+
+void postorder(node *n)
+{
+    node *temp = n;
+    while (!temp->traversed && temp)
+    {
+        if (temp->left && !temp->left->traversed)
+        {
+            temp = temp->left;
+        }
+        else if (temp->right && !temp->right->traversed)
+        {
+            temp = temp->right;
+        }
+        else
+        {
+            std::cout << temp->data << " ";
+            temp->traversed = true;
+            temp = n;
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    node *root = new node(6);
+    root->left = new node(3);
+    root->right = new node(13);
+    root->left->left = new node(8);
+    root->left->right = new node(11);
+    root->right->left = new node(4);
+    root->right->right = new node(9);
+    postorder(root);
+    return 0;
+}
